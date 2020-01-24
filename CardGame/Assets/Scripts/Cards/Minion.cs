11df496a969d940d63bf0f174469ec6fa05a86bf
Tadas Kalvaitis <tadas.k31@gmail.com>
2020-01-24 15:28:12 +0200

    remake cards in progress

---
+++ CardGame/Assets/Scripts/Cards/Minion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Minion", menuName = "Minion card")]
+public class Minion : ScriptableObject
+{
+    public string cardName;     //card's name
+    public string description;  //card's description
+    public int cost;            //card's cost
+    public Sprite image;        //card's image
+    public string type;         //card's type
+    public int attack;          //minion's attack
+    public int life;            //minion's life
+}    
---
+++ CardGame/Assets/Scripts/Cards/Spell.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Spell", menuName = "Spell card")]
+public class Spell : ScriptableObject
+{
+    public string cardName;
+    public string description;
+    public int cost;
+    public Sprite image;
+    public string type;
+}
