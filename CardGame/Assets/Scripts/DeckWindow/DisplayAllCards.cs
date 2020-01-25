3d04cde8e5faf86aadd0b0f226d7fa717ddefe4b
Tadas Kalvaitis <tadas.k31@gmail.com>
2020-01-25 11:17:31 +0200

    remake cards completed

---
+++ CardGame/Assets/Scripts/DeckWindow/DisplayAllCards.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DisplayAllCards : MonoBehaviour
+{
+    private int ROW_START = -516;       //x coordinates of first card in row
+    private int ROW_OFFSET = 344;       //offset for placing next card in row
+    private int LINE_OFFSET = 478;      //offset for placing cards in next line
+
+    public GameObject cardPrefab;       //card prefab
+    public Sprite minionBackground;     //minion card background
+    public Sprite spellBackground;      //spell card background
+    public RectTransform content;       //parent for cards 
+
+    private ScriptableObject[] cards;   //array that contains all cards
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gets all cards
+        ScriptableObject[] minions = Resources.LoadAll<Minion>("Cards/MinionCards");
+        ScriptableObject[] spells = Resources.LoadAll<Spell>("Cards/SpellCards");
+        cards = minions.Concat(spells).ToArray();
+
+        ////calculates height of content game object
+        float rows = (float)Math.Ceiling((minions.Length + spells.Length) / 4f);
+        float height = rows == 1 ? 24 * 2 + 455 : (24 * rows + 24) + (455 * rows);
+        content.sizeDelta = new Vector2(0, height);
+
+        int cardNumber = 1;     //card number in deck
+        foreach (var c in cards)
+        {
+            float row = (float)Math.Ceiling(cardNumber / 4f);           //row in which card is
+            float cardNumberInRow = cardNumber - ((row - 1) * 4) - 1;   //card number in row from 0 to 3
+            float x = ROW_START + ROW_OFFSET * cardNumberInRow;         //x position of card
+            float y = -251.5f - (row - 1) * LINE_OFFSET;                //y position of card
+
+            //spawns minion card
+            if (c.GetType().Name.ToLower().Equals("minion"))
+            {
+                Minion minion = (Minion)c;
+                minion.spawnCard(cardPrefab, spellBackground, content.gameObject, minion, x, y);
+            }
+            //spawns spell card
+            else
+            {
+                Spell spell = (Spell)c;
+                spell.spawnCard(cardPrefab, spellBackground, content.gameObject, spell, x, y);
+            }
+
+            cardNumber++;
+        }
+    }
+}
