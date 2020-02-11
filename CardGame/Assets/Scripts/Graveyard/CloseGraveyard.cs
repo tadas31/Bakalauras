7cd65b0b466c1a97ca59ce78f5182583d37805fb
Tadas Kalvaitis <tadas.k31@gmail.com>
2020-02-11 17:03:30 +0200

    Display used / killed cards in graveyard - in progress

---
+++ CardGame/Assets/Scripts/Graveyard/CloseGraveyard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseGraveyard : MonoBehaviour
+{
+    // Closes graveyard
+    public void onCLosePress()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ CardGame/Assets/Scripts/Graveyard/OpenGraveyard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OpenGraveyard : MonoBehaviour, IPointerClickHandler
+{
+    public Canvas cardsInGraveyard;         // Canvas for cards in graveyard
+
+    // Opens graveyard.
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        cardsInGraveyard.gameObject.SetActive(true);
+    }
+}
