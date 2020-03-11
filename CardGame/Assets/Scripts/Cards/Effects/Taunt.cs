16a6e16ab434063fcf8e1c97c13746fb80c1dffd
Tadas Kalvaitis <tadas.k31@gmail.com>
2020-03-11 13:57:41 +0200

    Taunt - completed

---
+++ CardGame/Assets/Scripts/Cards/Effects/Taunt.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Taunt : MonoBehaviour, IDescription
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Returns description of effect granted by this script
+    public string getDescription()
+    {
+        string description = "Taunt";
+        return description;
+    }
+}
